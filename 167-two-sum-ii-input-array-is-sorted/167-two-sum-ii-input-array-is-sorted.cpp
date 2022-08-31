283a87a26a0664b837d6141ae9fcd2503624eec3
Rajaram <rajaramsharma422@gmail.com>
2022-08-31 21:40:18 +0530

    Time: 15 ms (83.82%), Space: 15.5 MB (75.77%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
@@ -3,9 +3,9 @@ public:
     vector<int> twoSum(vector<int>& numbers, int target) {
         
         int comp = 0;
-        for(int i = 0; i < numbers.size(); i++){
-            comp = target - numbers[i];
-            if(binary_search(numbers.begin(), numbers.end(), comp)) return {i+1, (int)(upper_bound(numbers.begin(), numbers.end(), comp) - numbers.begin())};
+        for(int i = 1; i <= numbers.size(); i++){
+            comp = target - numbers[i-1];
+            if(binary_search(numbers.begin(), numbers.end(), comp)) return {i, (int)(upper_bound(numbers.begin(), numbers.end(), comp) - numbers.begin())};
         }
         return {-1, -1};
     }
