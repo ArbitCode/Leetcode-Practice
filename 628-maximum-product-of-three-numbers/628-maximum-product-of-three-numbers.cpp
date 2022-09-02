6eefa01383459bf792f297eecf18ebe610570305
Rajaram <rajaramsharma422@gmail.com>
2022-09-02 23:21:53 +0530

    Time: 51 ms (89.34%), Space: 27.8 MB (36.59%) - LeetHub

--- 628-maximum-product-of-three-numbers/628-maximum-product-of-three-numbers.cpp
+++ 628-maximum-product-of-three-numbers/628-maximum-product-of-three-numbers.cpp
@@ -1,13 +1,28 @@
 class Solution {
 public:
     int maximumProduct(vector<int>& nums) {
+        int min1 = INT_MAX, min2 = INT_MAX;
+        int max1 = INT_MIN, max2 = INT_MIN, max3 = INT_MAX;
+        for(auto num : nums){ 
+            if(num <= min1){ // num < min1, min2 
+                min2 = min1;
+                min1 = num; 
+            }else if(num <= min2){ // num between min1, min2
+                min2 = num;
+            }
+            if(num > max1){ // num > max1, max2, max3
+                max3 = max2;
+                max2 = max1;
+                max1 = num;
+            }else if(num >= max2){  //num between max1 , max2
+                max3 = max2;
+                max2 = num;
+            }else if(num>=max3){ //num between max2, max3
+                max3 = num;
+            }
+            
+        }
         
-        sort(nums.begin(), nums.end());
-        int n = nums.size();
-        int leftTwoAndOneRight = nums[0] *nums[1] *nums[n-1];
-        int rightThree = nums[n-1] * nums[n-2] * nums[n-3];
-        
-        return leftTwoAndOneRight < rightThree ? rightThree : leftTwoAndOneRight;
-        
+        return min1 * min2 * max1 < max1*max2*max3 ? max1*max2*max3 :min1 * min2 * max1;
     }
 };
