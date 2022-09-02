9972ec8579fd837b4768198ed6927f1b46b4c2e0
Rajaram <rajaramsharma422@gmail.com>
2022-09-02 17:42:16 +0530

    Time: 43 ms (80.47%), Space: 70.7 MB (99.47%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -11,41 +11,47 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        
         int count1 = 0, count2 = 0;
+        
         ListNode *ptr1 = l1, *ptr2 = l2;
         while(ptr1 || ptr2){
             if(ptr1) count1++, ptr1 = ptr1 -> next;
             if(ptr2) count2++, ptr2 = ptr2 -> next;
         }
-        delete ptr1;
-        delete ptr2;
+        
         ListNode *new_head = nullptr;
         if(count1 > count2) new_head = l1;
         else new_head = l2;
         
         ListNode *ptr = new_head;
         int carry = 0;
-        ListNode *ptrPrev = ptr;
+        ListNode *end = ptr;
+        
+        //till l1 == l2
         while(l1 && l2){
             int temp1 = l1 -> val;
             int temp2 = l2 -> val;
-            ptr -> val = (temp1 + temp2 + carry) % 10;
-            carry = (temp1 + temp2 + carry) / 10;
-            ptrPrev = (ptrPrev == ptr) ? ptr : ptrPrev->next;
+            int value = temp1 + temp2 + carry;
+            ptr -> val = value % 10;
+            carry = value / 10;
+            if(!ptr -> next) end = ptr;
             ptr = ptr -> next;
             l1 = l1 -> next;
             l2 = l2 -> next;
         }
-        if(ptr == nullptr && carry != 0) {ptrPrev -> next = new ListNode (carry); return new_head;}
+        // reached end but carry != 0 when l1 == l2
+        if(!end -> next && carry != 0) {end -> next = new ListNode (carry); return new_head;}
+        
+        // l1 != l2 => ptr pointest to largest list
         while(ptr){
-            int temp = ptr -> val;
-            ptr -> val = (temp + carry ) % 10;
-            carry = (temp + carry) / 10;
+            int value = ptr -> val + carry;
+            ptr -> val = value % 10;
+            carry = value / 10;
+            //reached end but carry != 0
             if(carry != 0 && !ptr->next) {ptr -> next = new ListNode (carry); return new_head;}
             ptr = ptr -> next;
         }
-        
-        //if(carry != 0) ptr = new ListNode(carry);
         
        return new_head; 
     }
