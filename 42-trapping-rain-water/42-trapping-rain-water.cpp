f00d0f1fbf9bdf2ab699e72406b56a25744feb3a
Rajaram <rajaramsharma422@gmail.com>
2022-09-02 14:32:22 +0530

    Time: 45 ms (5.87%), Space: 20.6 MB (11.61%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -1,37 +1,24 @@
 class Solution {
 public:
-    
-    int getLeftMaxHeight(vector<int> height, int index){
-        int leftMax = height[index];
-        for(int i = index; i >= 0; i--)
-            leftMax = max(leftMax, height[i]);
-        return leftMax;
-    }
-    
-    int getRightMaxHeight(vector<int> height, int index){
-        int rightMax = height[index];
-        for(int i = index; i < height.size(); i++)
-            rightMax = max(rightMax, height[i]);
-        return rightMax;
-    }
-    
     int trap(vector<int>& height) {
+        
+        int n = height.size();
+        
+        vector<int> leftMax(n);
+        vector<int> rightMax(n);
+        
+        leftMax[0] = height[0];
+        rightMax[n-1] = height[n-1];
+        
+        for(int i = 1; i < n; i++)
+            leftMax[i] = max(height[i], leftMax[i - 1]);
+        for(int i = n - 2; i >= 0; i--)
+            rightMax[i] = max(height[i], rightMax[i + 1]);
+        
         int ans = 0;
-        int size = height.size();
-        vector<int> dpLeftMax(size), dpRightMax(size);
-        dpLeftMax[0] = height[0];
-        for(int i = 1; i < size; i++)
-            dpLeftMax[i] = max(dpLeftMax[i - 1] , height[i]);
-        dpRightMax[size - 1] = height[size - 1];
-        for(int i = size - 2; i >= 0; i--)
-            dpRightMax[i] = max(dpRightMax[i + 1], height[i]);
-        for(int i = 1; i < height.size() - 1; i++)
-            ans += min(dpLeftMax[i], dpRightMax[i]) - height[i];
-        
-        //for(int i = 0; i < height.size(); i++)
-          //  ans += min(getLeftMaxHeight(height, i), getRightMaxHeight(height, i)) - height[i];
+        for(int i = 1; i < n-1; i++)
+            ans += min(leftMax[i], rightMax[i]) - height[i];
         return ans;
-        
-        
+            
     }
 };
