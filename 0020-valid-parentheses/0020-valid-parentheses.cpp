00f6aec2add7b707cc88077e2bc7546429ecc61d
Raja <rajaramsharma422@gmail.com>
2025-02-20 00:36:57 +0530

    Time: 0 ms (100%), Space: 8.8 MB (61.89%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -2,20 +2,18 @@ class Solution {
 public:
     bool isValid(string s) {
         std::stack<char> st1;
-        std::stack<char> st2;
 
         for (auto ch : s) {
             if (ch == '{' || ch == '[' || ch == '(') {
                 st1.push(ch);
             } else {
-                if (st1.size() == 0)
+
+                if (st1.size() == 0) {
                     return false;
+                }
                 char top = st1.top();
-                if (ch == ')' && top != '(')
-                    return false;
-                if (ch == ']' && top != '[')
-                    return false;
-                if (ch == '}' && top != '{')
+                if (ch == ')' && top != '(' || ch == ']' && top != '[' ||
+                    ch == '}' && top != '{')
                     return false;
                 st1.pop();
             }
